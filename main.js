9a27025d270c91cfcbeacfad29ef3ab898b6da32
Lynz951 <lindsay.haddix@student.awesomeinc.org>
2022-09-14 13:28:25 +0000

    debugging

--- main.js
+++ main.js
@@ -6,12 +6,6 @@ const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 
-const arr = [pic1.jpg, pic2.jpg, pic3.jpg, pic4.jpg, pic5.jpg];
-
-/* Declaring the alternative text for each image file */
-
-/* Declaring the array of image filenames */
-
 const images = ['pic1.jpg', `pic2.jpg`, `pic3.jpg`, `pic4.jpg`, `pic5.jpg`];
 const alts = {
   'pic1.jpg' : 'Closeup of a human eye',
